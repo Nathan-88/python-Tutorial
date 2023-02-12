89a97444a57c286bb6f4394409ea62207305d955
Onwuka Chukwuebuka Nathan <onwukaebuka88@gmail.com>
2023-02-12 19:24:38 +0100

    programming paradigms

--- Paradigms_.py
+++ Paradigms_.py
@@ -1,25 +1,18 @@
-# def fact(n):
-#     fac = 1
-#     for i in range(1,n+1):
-#         fac *= i
-#     return fac
-    
-# n = 6
-# print(fact(6))
-# print(dir(list))
-class MyList(list):
-    def __init__(self):
-        super().__init__()
+"""
+A programming paradigm is a style or approach to writing computer programs that emphasizes certain aspects of the software development process. It provides a set of guidelines, best practices, and techniques for solving problems and creating software.
 
-    def print_sorted(self):
-        print(sorted(self))
+There are several programming paradigms, including:
 
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)
+Imperative programming: This paradigm is based on the idea of giving the computer a sequence of tasks to perform, much like a set of instructions in a recipe. The programmer provides a step-by-step procedure for the computer to follow, and the computer carries out these instructions in order. Imperative programming is used in languages such as C, C++, and Java.
+
+Object-Oriented Programming (OOP): This paradigm is based on the concept of objects, which are instances of classes that encapsulate data and behavior. The main idea behind OOP is to organize code into objects that represent real-world entities, such as customers, products, or bank accounts. OOP is used in languages such as Java, Python, and Ruby.
+
+Functional programming: This paradigm is based on the idea of treating computation as the evaluation of mathematical functions. In functional programming, functions are first-class citizens, meaning they can be passed as arguments, assigned to variables, or returned as results. The focus is on using immutability, avoiding side effects, and reducing the use of state and mutable data. Functional programming is used in languages such as Haskell, Lisp, and Scheme.
+
+Logic programming: This paradigm is based on formal logic and the concept of relationships between objects. The programmer expresses the relationships between objects and the logic engine derives new relationships based on these. Prolog is the most well-known language used for logic programming.
+
+Scripting programming: This paradigm is often used for writing small programs to automate tasks. Scripting languages are interpreted, meaning that the code is executed line by line rather than compiled into machine code. Scripting languages are typically less complex and easier to learn than other programming languages, and are used for tasks such as web page scripting, system administration, and testing. Examples of scripting languages include Perl, Python, and Ruby.
+
+These are just a few examples of the many programming paradigms that exist. In practice, many programming languages support multiple paradigms, allowing programmers to use a combination of styles to solve problems in the way that best fits their needs.
+
+"""
