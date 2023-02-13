a68995819ac58592c455f8330a74e90b18c23fca
Onwuka Chukwuebuka Nathan <onwukaebuka88@gmail.com>
2023-02-13 07:27:52 +0100

    a function that divides all elements of a matrix.

--- Matrix_div.py
+++ Matrix_div.py
@@ -1,10 +1,29 @@
-"""A function that divides all the elements of the given matrix"""
+"""
+Prototype: def matrix_divided(matrix, div):
+matrix must be a list of lists of integers or floats, otherwise raise a TypeError exception with the message matrix must be a matrix (list of lists) of integers/floats
+Each row of the matrix must be of the same size, otherwise raise a TypeError exception with the message Each row of the matrix must have the same size
+div must be a number (integer or float), otherwise raise a TypeError exception with the message div must be a number
+div can’t be equal to 0, otherwise raise a ZeroDivisionError exception with the message division by zero
+All elements of the matrix should be divided by div, rounded to 2 decimal places
+Returns a new matrix
+"""
 
 
 def matrix_divided(matrix, div):
+    """
+    a function dives all elements in a matrix by div
+    """
     if type(matrix) is not list:
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(element, (int, float))
+               for row in matrix for element in row):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+
     n = len(matrix[0])
     for row in matrix:
         if len(row) != n:
