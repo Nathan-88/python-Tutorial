de6aba7f82951988d287f5e045a011fa89e7911b
Onwuka Chukwuebuka Nathan <onwukaebuka88@gmail.com>
2023-03-02 13:07:57 +0100

    finditer in regular expression

---
+++ Finditer.py
@@ -0,0 +1,18 @@
+"""
+finditer() is a method in Python's re (regular expression) module that searches for all occurrences of a pattern in a string and returns an iterator that yields match objects for each match.
+
+The syntax for using finditer() is: 
+re.finditer(pattern, string, flags=0)
+pattern: A regular expression pattern that you want to search for in the string.
+string: The string to be searched.
+flags: Optional flags that modify the behavior of the search. They are specified as constants in the re module, and can be combined using the bitwise OR operator (|).
+
+"""
+import re
+
+string = 'The quick brown fox jumps over the lazy dog.'
+pattern = r'\b\w{5}\b'  # Find all words that are 5 letters long
+
+for match in re.finditer(pattern, string):
+    print(
+        f"Found '{match.group()}' starting at index {match.start()} and ending at index {match.end()}.")
